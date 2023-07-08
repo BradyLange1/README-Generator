3652170cb823300349d6ca3f77b9959ff3f55c9c
BradyLange1 <bradylange99@gmail.com>
2023-07-08 10:38:08 -0500

    added license list prompt

--- index.js
+++ index.js
@@ -33,6 +33,22 @@ inquirer
             message: 'Enter test instructions: ',
             name: 'Tests'
         },
+        {
+            type: 'list',
+            message: 'What license would you like?',
+            choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Pulblic License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+            name: 'License'
+        },
+        {
+            type: 'input',
+            message: 'Enter your GitHub link: ',
+            name: 'Github'
+        },
+        {
+            type: 'input',
+            message: 'Enter your Email: ',
+            name: 'Email'
+        },
     ])
     .then((response) =>
     fs.writeFile('README.md',
@@ -48,6 +64,8 @@ ${response.Usage}
 ${response.Contributing}
 ## Tests: 
 ${response.Tests}
+## License: 
+${response.License}
 `
 ,
     (err) => err ? console.error(err) : console.log('Success!')
